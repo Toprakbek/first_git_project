b1d4dba7fb5def563a4afdc88769b236648eb2ac
mustafatoprak <mustafa80toprak@gmail.com>
2025-07-30 22:33:36 -0500

    I push a message to Git Hub

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,5 +3,6 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+        System.out.println("I am pushing this message to Git Hub");
     }
 }
