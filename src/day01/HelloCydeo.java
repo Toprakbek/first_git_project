de0e80600192993c2fd1c4b1457a9c8962513fbb
Toprakbek <mustafa80toprak@gmail.com>
2025-07-30 22:35:16 -0500

    Update HelloCydeo.java
    Pulling from Git Hub

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,6 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("I am pushing this message to Git Hub");
+        System.out.println("I am pulling this message from Git Hub");
     }
 }
